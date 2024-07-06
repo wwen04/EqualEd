c71868cf1d6ac61b7815523c93046902d7bfb1dc
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-07-06 13:13:16 +0800

    chore: Remove unused useState import in header component

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 
 export default function Header() {
   const pathname = usePathname();
