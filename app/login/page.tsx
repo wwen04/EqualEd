03841fcbc331a02fedd8ce8fd03b7d29bec8f5ee
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-07-07 02:27:50 +0800

    created login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import LoginForm from "./loginForm";
 
 export default function Login() {
-    return (
-        <div className="grid items-center justify-center w-full h-[calc(100vh-12rem)]">
-            <LoginForm/>
-        </div>
-    );
+  return (
+    <div className="grid items-center justify-center w-full h-screen bg-login-bg bg-cover bg-center">
+      <LoginForm />
+    </div>
+  );
 }
