ef42118a7f71d6e4a261eccc060c6491cad8b60d
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-07-06 21:56:34 +0800

    created resources page

---
+++ app/resources/ResourcesPage.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+function ResourcesPage() {
+  <main>
+    <h1>Resources</h1>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Pages</th>
+        <th>Created On</th>
+        <th>Owner</th>
+      </tr>
+      <tr>
+        <td>FundamentalsofPython.pdf</td>
+        <td>24</td>
+        <td>12/05/2024</td>
+        <td>Chua Hock Seng</td>
+      </tr>
+      <tr>
+        <td>DataStructures.pdf</td>
+        <td>240</td>
+        <td>16/05/2024</td>
+        <td>Addison Wesley</td>
+      </tr>
+    </table>
+  </main>;
+}
+
+export default ResourcesPage;
