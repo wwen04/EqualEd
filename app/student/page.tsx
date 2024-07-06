60decb09c759e9f8152b956076c2ff68e97c1ba4
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-07-06 13:12:52 +0800

    chore: Add header component to layout and student page

---
+++ app/student/page.tsx
@@ -0,0 +1,31 @@
+export default function StudentPage() {
+  return (
+    <main className="m-8 mx-16">
+      <section>
+        <div className="flex justify-between border-b-2 border-gray-200 pb-4">
+          <h1 className="text-3xl font-bold font-mono">My Courses</h1>
+          <div className="flex gap-2">
+            <button
+              type="button"
+              className="p-3 rounded-full font-semibold bg-green-700 text-white"
+            >
+              Default
+            </button>
+            <button
+              type="button"
+              className="p-3 rounded-full font-semibold border-gray-200 border"
+            >
+              A - Z
+            </button>
+            <button
+              type="button"
+              className="p-3 rounded-full font-semibold border-gray-200 border"
+            >
+              List View
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
